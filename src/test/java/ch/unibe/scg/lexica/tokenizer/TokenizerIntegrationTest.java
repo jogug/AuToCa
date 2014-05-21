426eddca855e46db83bd75ae01ca56a4cf71f8c9
Jan Kurs <kurs.jan@gmail.com>
2014-05-21 08:58:25 +0200

    Test for data_file.c

---
+++ src/test/java/ch/unibe/scg/lexica/tokenizer/TokenizerIntegrationTest.java
@@ -0,0 +1,25 @@
+package ch.unibe.scg.lexica.tokenizer;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.junit.Test;
+
+public class TokenizerIntegrationTest {
+	
+	
+	@Test
+	public void testDataFile() throws ClassNotFoundException, URISyntaxException {
+		URL fileUrl = this.getClass().getResource("resources/data_file.c");
+		MockHandler mh = new MockHandler();
+		Tokenizer tokenizer = new Tokenizer(mh);
+		
+		assertNotNull(fileUrl);
+		tokenizer.tokenize(new File(fileUrl.toURI()));
+		
+		assertTrue(mh.tokens.size() > 0);
+	}
+}
