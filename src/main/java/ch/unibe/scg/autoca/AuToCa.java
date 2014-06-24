05ca5b4b698dfc7b8a4a53b2215eceb0a5aa5d2c
Jan Kurs <kurs@iam.unibe.ch>
2014-06-24 16:56:57 +0200

    Code removed from a constructor

--- src/main/java/ch/unibe/scg/autoca/AuToCa.java
+++ src/main/java/ch/unibe/scg/autoca/AuToCa.java
@@ -1,27 +1,33 @@
 /*
-** Copyright 2013 Software Composition Group, University of Bern. All rights reserved.
-*/
+ ** Copyright 2013 Software Composition Group, University of Bern. All rights reserved.
+ */
 package ch.unibe.scg.autoca;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ch.unibe.scg.autoca.mode.ScanMode;
+
 public final class AuToCa {
 
-    private static final Logger logger = LoggerFactory.getLogger(AuToCa.class);
+	private static final Logger logger = LoggerFactory.getLogger(AuToCa.class);
 
-    /** TODO JK: bad practice, to keep this in a constructor... */
-    public AuToCa(String[] args) {
-    	DataSet dataset = new DataSet();
-    	dataset.loadStandardDataSet();;
-    	
-    	ScanMode scanmode = new ScanMode();
-    	scanmode.execute(dataset);
-    }
+	private DataSet dataset;
 
-    public static void main(String[] args) {
-		@SuppressWarnings("unused")
+	public AuToCa(String[] args) {
+		dataset = new DataSet();
+	}
+
+	public void run() {
+		dataset.loadStandardDataSet();
+
+		ScanMode scanmode = new ScanMode();
+		scanmode.execute(dataset);
+	}
+
+	public static void main(String[] args) {
 		AuToCa autoca = new AuToCa(args);
-    }
+		autoca.run();
+
+	}
 }
