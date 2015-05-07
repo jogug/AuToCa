1ee662876297faa41cc82a172070fc56f9963631
Joel <guggisberg8@gmail.com>
2015-05-07 12:02:14 +0200

    data

--- src/main/java/ch/unibe/scg/autoca/AuToCa.java
+++ src/main/java/ch/unibe/scg/autoca/AuToCa.java
@@ -20,7 +20,7 @@ public final class AuToCa {
 	}
 
 	public static void main(String[] args) {	
-		String[] x = {"analyze", "path", "resources/configuration/java.cfg"};
+		String[] x = {"analyze", "path", "resources/configuration/clojure.cfg"};
 		AuToCa autoca = new AuToCa(x);
 	}
 
